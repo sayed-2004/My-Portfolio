77204563b365d9440ddad373829fe0e88b1a7ce2
sayed-2004 <sayediqbalsadat148@gmail.com>
2025-03-02 22:55:26 +0000

    navbar design

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <ul className='nav-menu'>
-            <li>
-            <a className="nav-link active" href="#">Home</a>
-            </li>
+            <li>Home</li>
             <li>About Me</li>
             <li>Services</li>
             <li>Portfolio</li>
