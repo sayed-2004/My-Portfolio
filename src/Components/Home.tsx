b22f422cf7ee72bb16b4a7466da527660b04fe92
sayed-2004 <sayediqbalsadat148@gmail.com>
2025-03-05 22:58:49 +0000

    image design

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -8,13 +8,9 @@ const Home = () => {
         <div className="home-container">
             <div className="text-content">
                 <h1>Sayed Iqbal Sadat</h1>
-
                 <br />
-
                 <h3>CS First-Year Undergraduate Student</h3>
-
                 <br />
-                
                 <p>I'm a first-year Computer Science student with a passion 
                   for coding, problem-solving, and all things tech. I love building 
                   cool projects, exploring new technologies, and constantly challenging 
