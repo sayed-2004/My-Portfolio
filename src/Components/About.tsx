5cdb175d68ed1d95582de05fb8f0eb9ffb167b92
sayed-2004 <sayediqbalsadat148@gmail.com>
2025-03-11 15:08:03 +0000

    about page design completed

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -3,18 +3,18 @@ import React from 'react'
 const About = () => {
   return (
     <div className='w-full h-screen'>
-      <div>
-        <div>
-            <p>About</p>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
         </div>
 
-        <p>
+        <p className='text-xl mt-20'>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae dicta architecto voluptatum accusantium laudantium veniam, repudiandae rem saepe velit vel amet quaerat laboriosam unde nihil adipisci ex consequatur modi dolor autem error, minima facere magnam cupiditate! Culpa laudantium ut quaerat sequi! Minima, quam. Adipisci vel aperiam incidunt laborum dolorum accusantium?
         </p>
 
         <br />
 
-        <p>
+        <p className='text-xl'>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora in nostrum sed, harum voluptate dignissimos minus est doloremque doloribus dolore tempore placeat illo rem mollitia minima culpa velit illum eum incidunt libero ex magni eveniet ducimus? Quod laudantium iste doloribus porro sunt quos, consectetur, eveniet ipsum cumque quidem doloremque at?
         </p>
       </div>
